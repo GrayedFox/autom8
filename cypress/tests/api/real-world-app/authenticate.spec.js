fa432322f6a26cdfe90871cec0988e473fda0897
Che Fisher <GrayedFox@users.noreply.github.com>
2021-06-10 11:12:34 +0200

    update readme, add mongodb dep, add docker install and node prereq links
    add seed, setup, and teardown tasks, WIP generate fixtures task, update cypress, update git ignore
    
    add waitForIt script, WIP get mongodb connection working

--- cypress/tests/api/real-world-app/authenticate.spec.js
+++ cypress/tests/api/real-world-app/authenticate.spec.js
@@ -1,9 +1,16 @@
+const userData = require('../../../../database/seedData/users.json')
+
 describe('Authentication - API', () => {
   context('Authenticates with a valid JSON payload', () => {
     let loginResponse
 
     before(() => {
       cy.loginByJSON().then((response) => { loginResponse = response })
+      cy.task('db:logTotal')
+      cy.task('db:seed', userData.users)
+      cy.task('db:logTotal')
+      cy.task('db:generateFixtures')
+      // cy.task('db:generateFixtures')
     })
 
     it('Returns a 200 response with a valid JSON payload', () => {
